4f71bb5e4ca52cba677d4ad155c034e5b669cd8d
aricasas <ari.casas.nassar@mail.utoronto.ca>
2023-07-26 17:13:07 -0400

    Implemented SessionLoadC (the controller)

--- src/main/java/views/session_load/SessionLoadC.java
+++ src/main/java/views/session_load/SessionLoadC.java
@@ -1,4 +1,29 @@
 package views.session_load;
 
+import use_cases.session_load.SessionLoadIB;
+import use_cases.session_load.SessionLoadID;
+
 public class SessionLoadC {
+    private final SessionLoadIB interactor;
+
+    public SessionLoadC(SessionLoadIB interactor) {
+        this.interactor = interactor;
+    }
+
+    /**
+     * Calls the interactor to load a new session from a file
+     * @param filename the filename to load the session from
+     */
+    public void loadFile(String filename) {
+        SessionLoadID inputData = new SessionLoadID(filename);
+        this.interactor.load(inputData);
+    }
+
+    /**
+     * Calls the interactor to create a new empty session
+     */
+    public void loadNew() {
+        SessionLoadID inputData = new SessionLoadID();
+        this.interactor.load(inputData);
+    }
 }
