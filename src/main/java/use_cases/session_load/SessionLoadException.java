dc68137c534b3e55daaed69ea3e970a4c76d119b
aricasas <ari.casas.nassar@mail.utoronto.ca>
2023-07-29 17:48:43 -0400

    Improve SessionLoadException JavaDoc

--- src/main/java/use_cases/session_load/SessionLoadException.java
+++ src/main/java/use_cases/session_load/SessionLoadException.java
@@ -2,7 +2,7 @@ package use_cases.session_load;
 
 public class SessionLoadException extends Exception {
     /**
-     * Creates a new SessionLoadException for reporting exceptions specific to the this use case.
+     * An exception used when there was an issue loading a session from a file
      */
     public SessionLoadException(String message) {
         super(message);
