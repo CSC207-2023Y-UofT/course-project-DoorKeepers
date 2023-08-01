549571640a35e3e1f833977ebb859a9726cea2a8
katarinaavucic <kavucic@icloud.com>
2023-08-01 12:02:04 -0400

    Fixed punctuation in SessionStorageTest

--- src/test/java/entities/SessionStorageTest.java
+++ src/test/java/entities/SessionStorageTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 
 /**
- * A class that tests the SessionStorage class
+ * A class that tests the SessionStorage class.
  */
 class SessionStorageTest {
 
