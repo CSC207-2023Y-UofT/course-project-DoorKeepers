15a1acc809f852e07162bcd56971f1f888cf1fec
katarinaavucic <kavucic@icloud.com>
2023-08-01 21:57:31 -0400

    Modified GenerateSummaryP to allow for a negative remainder (user spending over their budget, made a mistake on naming of last commit)

--- src/main/java/views/generate_summary_views/GenerateSummaryP.java
+++ src/main/java/views/generate_summary_views/GenerateSummaryP.java
@@ -8,9 +8,13 @@ import javax.swing.*;
 import org.jfree.chart.plot.CategoryPlot;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.chart.renderer.category.BarRenderer;
+import org.jfree.chart.title.TextTitle;
+import org.jfree.chart.title.Title;
 import org.jfree.data.category.CategoryDataset;
 import org.jfree.data.category.DefaultCategoryDataset;
 
+import org.jfree.data.general.DefaultPieDataset;
+import org.jfree.data.general.PieDataset;
 import use_cases.generate_summary_use_case.GenerateSummaryOB;
 import use_cases.generate_summary_use_case.GenerateSummaryOD;
 
@@ -19,25 +23,25 @@ import java.util.Map;
 
 /**
  * GenerateSummaryP: A class that generates the graphs
- * Code for Bar Graph inspired by http://www.java2s.com/Code/Java/Chart/JFreeChartStackedBarChartDemo4.htm
- * Code for Pie Chart inspired by ...
+ * Code for Bar Graph inspired by <a href="http://www.java2s.com/Code/Java/Chart/JFreeChartStackedBarChartDemo4.htm">this website.</a>
+ * Code for Pie Chart inspired by <a href="https://www.tutorialspoint.com/jfreechart/jfreechart_pie_chart.htm">this website.</a>
  * @author katarinavucic
  */
 public class GenerateSummaryP extends JFrame implements GenerateSummaryOB {
-    private double monthlybudget;
+    private double remainder;
     private Map<String, ArrayList<Double>> statisticalData;
 
     /**
      * Generates a graphical representation of the formatted data associated with the current month and modifies the
-     * outputData to contain the resulting JPanel.
+     * outputData to contain the resulting JPanels.
      * @param outputData a GenerateSummaryOD object holding the formatted data needed to generate the graphs
      */
     @Override
     public void formatOutputData(GenerateSummaryOD outputData) {
-        this.monthlybudget = outputData.getMonthlyBudget();
+        this.remainder = outputData.getRemainder();
         this.statisticalData = outputData.getStatisticalData();
-
-        outputData.setGraphPanel(generateBarGraph());
+        outputData.setBarGraphPanel(generateBarGraph());
+        outputData.setPieChartPanel(generatePieChart());
     }
 
     /**
@@ -54,6 +58,23 @@ public class GenerateSummaryP extends JFrame implements GenerateSummaryOB {
     }
 
     /**
+     * Generates the Pie Chart
+     * @return a JPanel object holding the Pie Chart
+     */
+    private JPanel generatePieChart() {
+        PieDataset pieChartDataset = createPieChartDataset();
+        JFreeChart pieChart = ChartFactory.createPieChart("Monthly Expenses", pieChartDataset, true, true, false);
+        Title subtitle;
+        if (this.remainder >= 0){
+            subtitle = new TextTitle("You are within budget.");
+        } else {
+            subtitle = new TextTitle("You are over budget.");
+        }
+        pieChart.addSubtitle(subtitle);
+        return new ChartPanel(pieChart);
+    }
+
+    /**
      * Generates the dataset used to plot the Bar Graph
      * @return a CategoryDataset object holding the data
      */
@@ -64,6 +85,21 @@ public class GenerateSummaryP extends JFrame implements GenerateSummaryOB {
             barGraphDataset.addValue(this.statisticalData.get(c).get(1), "Budget", c);
         }
         return barGraphDataset;
+    }
+
+    /**
+     * Generates the dataset used to plot the Pie Chart
+     * @return a PieDataset object holding the data
+     */
+    private PieDataset createPieChartDataset() {
+        DefaultPieDataset pieChartDataset = new DefaultPieDataset();
+        for (String c: this.statisticalData.keySet()){
+            pieChartDataset.setValue(c, this.statisticalData.get(c).get(0));
+        }
+        if (this.remainder >= 0) {
+            pieChartDataset.setValue("Unspent", this.remainder);
+        }
+        return pieChartDataset;
     }
 
 }
