35c4e6e940dd009b64ff5e2631073bde8aac92a7
aricasas <ari.casas.nassar@mail.utoronto.ca>
2023-08-02 02:17:57 -0400

    Rewrote the success test case for FileSessionStorageTest and added tests for fail cases too.

--- src/test/java/views/file_session_storage/FileSessionStorageTest.java
+++ src/test/java/views/file_session_storage/FileSessionStorageTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.io.IOException;
 
 /**
  * Tests the FileSessionStorage class
@@ -23,20 +24,17 @@ class FileSessionStorageTest {
 
 
     /**
-     * Tests the saving and loading functionality of FileSessionStorage
+     * Tests the successful saving and loading functionality of FileSessionStorage
      * This isn't split into two test cases because they are heavily intertwined; we can't test loading
-     * a file without saving one first too.
+     * a file without saving one first too. And we can't test that a save was successful without
+     * loading it back to check.
      */
     @Test
-    public void FileSessionStorage() {
+    public void FileSessionStorage() throws IOException, ClassNotFoundException {
         FileSessionStorage fileSessionStorage = new FileSessionStorage();
 
         // Save session into file
-        try {
-            fileSessionStorage.save("FSSSaveFileTest.ser", sampleSession);
-        } catch (Exception e) {
-            Assertions.fail();
-        }
+        fileSessionStorage.save("FSSSaveFileTest.ser", sampleSession);
 
         // Check file exists
         File f = new File("FSSSaveFileTest.ser");
@@ -44,16 +42,39 @@ class FileSessionStorageTest {
         Assertions.assertTrue(f.isFile());
 
         // Load session from file
-        SessionStorage savedAndLoadedSession = null;
-        try {
-            savedAndLoadedSession = fileSessionStorage.load("FSSSaveFileTest.ser");
-        } catch (Exception e) {
-            Assertions.fail();
-        }
+        SessionStorage savedAndLoadedSession = fileSessionStorage.load("FSSSaveFileTest.ser");
 
+        // Check session loaded is correct
         Assertions.assertEquals(sampleSession, savedAndLoadedSession);
 
         // Cleanup
         new File("FSSSaveFileTest.ser").delete();
+    }
+
+    /**
+     * Tests that the load method will throw an exception when loading a file that doesn't exist
+     */
+    @Test
+    public void FileSessionStorageLoadNonexistentFile() {
+        FileSessionStorage fileSessionStorage = new FileSessionStorage();
+
+        Assertions.assertThrows(IOException.class, () -> fileSessionStorage.load("TestNonexistentFile.ser"));
+    }
+
+    /**
+     * Tests that the load method will throw an exception when loading a file that is not a serialization
+     * of a SessionStorage
+     */
+    @Test
+    public void FileSessionStorageLoadInvalidFile() throws IOException {
+        FileSessionStorage fileSessionStorage = new FileSessionStorage();
+
+        // Build an invalid file to load
+        new File("Hello world.txt").createNewFile();
+
+        Assertions.assertThrows(IOException.class, () -> fileSessionStorage.load("Hello world.txt"));
+
+        // Cleanup
+        new File("Hello world.txt").delete();
     }
 }
