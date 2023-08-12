f656759219806f1994b5ff72a4c0958d6fbbc203
katarinaavucic <kavucic@icloud.com>
2023-08-12 00:31:01 -0400

    Revert "Created the factory design pattern for creating expenses and categories. Modified Category and Expense to extend MonthObject. Added abstract MonthObjectCreator and concrete classes to create MonthObject classes. Added abstract MonthObjectCreatorInputData and concrete classes to pass in information to creator classes."
    This reverts commit ec1820ed

--- src/main/java/entities/Expense.java
+++ src/main/java/entities/Expense.java
@@ -5,9 +5,8 @@ import java.util.Objects;
 
 /**
  * A class that will store all the data associated with an Expense. This includes a name, Category, and value.
- * It implements the MonthObject interface, which allows for it to be created using a MonthObjectCreator.
  */
-public class Expense implements MonthObject, Serializable {
+public class Expense implements Serializable {
 
     private String name;
     private Category category;
--- src/main/java/entities/MonthObject.java
+++
@@ -1,7 +0,0 @@
-package entities;
-
-/**
- * An interface that objects belonging to the MonthlyStorage implement. Expense and Category implement this so that
- * they can be created by MonthObjectFactory classes.
- */
-public interface MonthObject {}
